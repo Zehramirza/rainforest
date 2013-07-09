234c148f9a6520662ff76d294436cd8f223eddfe
Ameetwadhwani <ameet.wadhwani@gmail.com>
2013-07-09 15:27:20 -0400

    committing the ameet.js file

---
+++ app/assets/javascripts/ameet.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: admin
+ * Date: 2013-07-09
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+//I made this!
