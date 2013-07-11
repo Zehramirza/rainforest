7df1ffbef1f9b44b1b74c874858959e629e5acab
Zehramirza <zehrabeg@gmail.com>
2013-07-10 22:16:30 -0400

    Displaying validation errors on the product form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+
+  validates :description, :name, :presence => true
+  validates :price_in_cents, :numericality => {:only_integer => true}
 end
+
