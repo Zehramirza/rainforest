20f40f5ca5ccdd74343b353f034496fd12b917ae
Zehramirza <zehrabeg@gmail.com>
2013-07-11 18:07:30 -0400

    Adding logic to display - this time it works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	   user = User.find_by_email(params[:email])
+  	user = User.find_by_email(params[:email])
+    
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to products_url, :notice => "Logged in!"
