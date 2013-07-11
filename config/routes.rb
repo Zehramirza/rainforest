9ad2e34c78d655c6e8c3300e7dfa810305e03daa
Zehramirza <zehrabeg@gmail.com>
2013-07-11 16:35:05 -0400

    Creating log-in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rainforest::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   get "users/new"
 
   get "users/create"
 
   resources :products
+
+  resources :users, :only => [:new, :create]
+
+
+  resources :sessions, :only => [:new, :create, :destroy]
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
